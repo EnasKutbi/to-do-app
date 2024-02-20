5f9b2421336f4fdf61c2b260be0b1fad068c3313
EnasKutbi <enaskutbi@gmail.com>
2024-02-21 02:47:05 +0300

    Basic Requirements

---
+++ index.js
@@ -0,0 +1,100 @@
+//
+
+const todosList = document.querySelector(".todos-list");
+const addButton = document.querySelector("#add-btn");
+const todoInput = document.querySelector("#todo-input");
+
+const todos = [
+  { description: "learn HTML", completed: false },
+  { description: "learn CSS", completed: false },
+  { description: "learn JS", completed: false },
+];
+
+function displayTodos(todos) {
+  todosList.innerHTML = "";
+  if (todos.length === 0) {
+    console.log("No todos found");
+  } else {
+    /*for (const todo of todos) {
+      console.log(todo.description);
+    }*/
+    for (let index = 0; index < todos.length; index++) {
+      //creat a div todoItem
+      const todoItem = document.createElement("div");
+      todoItem.classList.add("todo");
+
+      //add checkbox to todoItem
+      const todoCheckbox = document.createElement("input");
+      todoCheckbox.type = "checkbox";
+      todoCheckbox.checked = todos[index].completed;
+      todoItem.appendChild(todoCheckbox);
+
+      //creat and add description to todoItem
+      const todoDescription = document.createElement("p");
+      todoDescription.textContent = todos[index].description;
+      todoItem.appendChild(todoDescription);
+
+      //creat and add delete button to todoItem
+      const todoDeleteButton = document.createElement("button");
+      todoDeleteButton.textContent = "Delete";
+      todoDeleteButton.addEventListener("click", () => deletToDO(index));
+      todoItem.appendChild(todoDeleteButton);
+
+      //creat and add edit button to todoItem
+      const todoEditButton = document.createElement("button");
+      todoEditButton.textContent = "Edit";
+      todoItem.appendChild(todoEditButton);
+
+      todosList.appendChild(todoItem);
+    }
+  }
+}
+
+function addToDO() {
+  const todoDescription = todoInput.value.trim();
+  const newTodo = {
+    description: todoDescription,
+    completed: false,
+  };
+  todos.push(newTodo);
+  displayTodos(todos);
+}
+function updateTodo() {}
+function eidtToDO() {}
+
+function deletToDO(index) {
+  todos.splice(index, 1);
+  displayTodos(todos);
+}
+
+addButton.addEventListener("click", addToDO);
+
+displayTodos(todos);
+
+/*
+function download() {
+    alert('downloaded');
+}
+const downloadButton = document.getElementById('download-btn');
+downloadButton.addEventListener("click", download);
+*/
+
+/*
+const heading1 = document.getElementsByTagName("h1") [0];
+console.log(heading1);
+heading1.textContent = "My List";
+heading1.style.color = "red";
+*/
+//heading1.classList.remove("title");
+/*
+const heading1 = document.getElementById('add-button');
+heading1.textContent = "Add";
+*/
+
+/* 
+const heading1 = document.querySelector("#id");
+const heading1 = document.querySelector(".class");
+getElementsByClassName();
+getElementsByTagName();
+getElementById();
+*/
